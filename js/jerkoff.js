8eeb4aaf6846e8e197ca60015ca14ec400edb938
yukio808 <aj.yamamoto@me.com>
2015-01-17 15:58:24 -1000

    added arrays

--- js/jerkoff.js
+++ js/jerkoff.js
@@ -1,14 +1,47 @@
-function jackpot(){
-    var slot1 = document.getElementById("slot1");
+function jackpot(str){
+    // acceses slot1
+    /*var slot1 = document.getElementById("slot1");
     var slot2 = document.getElementById("slot2");
     var slot3 = document.getElementById("slot3");
-    var status = document.getElementById("status");
-    var s1 = ([]);
-    var s2 = ([]);
-    var s3 = ([]);
+    var status = document.getElementById("status");*/
+    var s1 = (
+        "annually",
+        "daily",
+        "firmly",
+        "underground",
+        "always",
+        "before",
+        "certain",
+        "fast",
+        "hard",
+        "so");
+    var s2 = (
+        "large",
+        "fresh",
+        "bitchin\'",
+        "damned",
+        "sexy",
+        "wee",
+        "wonky",
+        "bullshit",
+        "mothafuckin\'",
+        "horny");
+    var s3 = (
+        "ass",
+        "whooping",
+        "cap\'n",
+        "matey",
+        "samurai",
+        "douchebagery",
+        "fucker",
+        "stripper heels",
+        "fuck up",
+        "ninja",
+        "laser beams");
+        
     var diceTotal = d1 + d2 + d3;
-    slot1.innerHTML = d1;
-    slot2.innerHTML = d2;
-    slot3.innerHTML = d3;
-    status.innerHTML = "You rolled "+diceTotal+".";
+    slot1.innerHTML = s1;
+    slot2.innerHTML = s2;
+    slot3.innerHTML = s3;
+    status.innerHTML = diceTotal;
 }
